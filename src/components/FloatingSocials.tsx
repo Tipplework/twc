a95558719f140b38c464b1715eb48628bcb8bb0b
Tipplework <Rohandhirwani13@gmail.com>
2025-05-10 15:16:38 +0530

    Update FloatingSocials.tsx

--- src/components/FloatingSocials.tsx
+++ src/components/FloatingSocials.tsx
@@ -3,15 +3,15 @@ import { Instagram, Linkedin } from 'lucide-react';
 
 const FloatingSocials = () => {
   return (
-    <div className="fixed bottom-4 left-4 z-50 flex flex-col gap-3 md:bottom-6 md:left-6">
+    <div className="fixed bottom-6 left-6 z-50 hidden md:flex flex-col gap-2">
       <a
         href="https://www.instagram.com/tippleworksco/"
         target="_blank"
         rel="noopener noreferrer"
         className="group"
       >
-        <div className="bg-black p-2 rounded-full hover:bg-tipple-yellow transition md:p-3">
-          <Instagram className="text-white w-4 h-4 md:w-5 md:h-5" />
+        <div className="bg-black p-1.5 rounded-full hover:bg-tipple-yellow transition">
+          <Instagram className="text-white w-3 h-3" />
         </div>
       </a>
       <a
@@ -20,8 +20,8 @@ const FloatingSocials = () => {
         rel="noopener noreferrer"
         className="group"
       >
-        <div className="bg-black p-2 rounded-full hover:bg-tipple-purple transition md:p-3">
-          <Linkedin className="text-white w-4 h-4 md:w-5 md:h-5" />
+        <div className="bg-black p-1.5 rounded-full hover:bg-tipple-purple transition">
+          <Linkedin className="text-white w-3 h-3" />
         </div>
       </a>
     </div>
