210ab583a039c0bdd2dfde6636c90053abbe9b17
Tipplework <Rohandhirwani13@gmail.com>
2025-05-12 13:54:45 +0530

    Update Work.tsx

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import FloatingSocials from '../components/FloatingSocials';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import { FloatingSocials } from '../components/FloatingSocials';
 
 const workData = [
   {
