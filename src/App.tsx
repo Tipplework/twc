a4f250c41bc1336983e4820a9fd3b7db2bb8a385
Tipplework <Rohandhirwani13@gmail.com>
2025-06-16 17:37:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ComingSoon from './ComingSoon';
+import ProjectDetail from "./pages/ProjectDetail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Work from "./pages/Work";
+import Services from "./pages/Services";
+import Contact from "./pages/Contactinnerpage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<ComingSoon />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project/:slug" element={<ProjectDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
