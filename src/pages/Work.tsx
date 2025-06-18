6536d1d0bcaab0f5c252df9e865db47f8b44e732
Tipplework <Rohandhirwani13@gmail.com>
2025-06-18 19:49:04 +0530

    Update Work.tsx

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -53,7 +53,7 @@ export default function Work() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-24">
         {filteredProjects.map((project) => (
           <Link key={project.slug} to={`/project/${project.slug}`}>
             <motion.div
